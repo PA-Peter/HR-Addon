53c22728db74275a6705548ce57348fe497a2549
PA-Peter <peter.rieck@rieckmedia.com>
2026-09-25 17:04:56 +0200

    Improve Weekly Working Hours list view and permissions

---
+++ hr_addon/hr_addon/doctype/weekly_working_hours/weekly_working_hours_list.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2026, RieckMedia and contributors
+// See license.txt
+
+frappe.listview_settings["Weekly Working Hours"] = {
+	add_fields: [
+		"employee_name",
+		"valid_from",
+		"valid_to",
+		"total_work_hours",
+		"set_target_hours_to_zero_when_date_is_holiday",
+		"disable_minimum_break_rule",
+	],
+};
